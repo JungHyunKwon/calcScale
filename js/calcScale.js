2051843a245cc9f71fb96ed77ca485cfa96c6dc1
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-09-05 13:04:37 +0900

    published

--- js/calcScale.js
+++ js/calcScale.js
@@ -7,50 +7,6 @@ try {
 		'use strict';
 
 		/**
-		 * @name 형태얻기
-		 * @since 2017-12-06
-		 * @param {*} value
-		 * @return {string || undefined}
-		 */
-		function _getType(value) {
-			var result;
-			
-			//매개변수가 있을때
-			if(arguments.length) {
-				//null일때
-				if(value === null) {
-					result = 'null';
-				
-				//undefined일때
-				}else if(value === undefined) {
-					result = 'undefined';
-				}else{
-					result = Object.prototype.toString.call(value).toLowerCase().replace('[object ', '').replace(']', '');
-					
-					//Invalid Date일때
-					if(result === 'date' && isNaN(new Date(value))) {
-						result = 'Invalid Date';
-					
-					//숫자일때
-					}else if(result === 'number') {
-						//NaN일때
-						if(isNaN(value)) {
-							result = 'NaN';
-						
-						//Infinity일때
-						}else if(!isFinite(value)) {
-							result = value.toString();
-						}
-					}else if(result === 'console') {
-						result = 'object';
-					}
-				}
-			}
-
-			return result;
-		}
-
-		/**
 		 * @name 확대 계산
 		 * @since 2018-09-05
 		 * @param {number} from
@@ -63,26 +19,24 @@ try {
 				distance : 0
 			};
 			
-			//숫자일때
-			if(_getType(from) === 'number' && _getType(to) === 'number') {
-				var ratio = from / to;
+			//숫자형 변환
+			from = parseFloat(from, 10);
+			to = parseFloat(to, 10);
 
-				//값이 같지 않을때
-				if(from !== to) {
-					result.scale /= ratio;
+			//from이 0이상이면서 to가 0이상일때
+			if(from >= 0 && to >= 0) {
+				result.scale /= from / to;
 
-					//소수점 2자리 뒤에 자르기
-					result.scale = parseFloat(result.scale.toFixed(2), 10);
+				//NaN 또는 Infinity일때
+				if(isNaN(result.scale) || result.scale.toString() === 'Infinity') {
+					result.scale = to;
+				}
 
-					//from이 초과일때
-					if(from > to) {
-						result.distance = -(from - to);
-					}else{
-						result.distance = to - from;
-					}
-					
-					//소수점 2자리 뒤에 자르기
-					result.distance = parseFloat(result.distance.toFixed(2), 10);
+				//from이 초과일때
+				if(from > to) {
+					result.distance = -(from - to);
+				}else{
+					result.distance = to - from;
 				}
 			}
 
