026d1f8d41b2c68e21f03899c75b6ee44acc4d57
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-11-19 01:39:11 +0900

    published

--- js/calcScale.js
+++ js/calcScale.js
@@ -7,6 +7,46 @@ try {
 		'use strict';
 
 		/**
+		 * @name 정수 확인
+		 * @since 2017-12-06
+		 * @param {*} value
+		 * @return {boolean}
+		 */
+		function _isInt(value) {
+			return typeof value === 'number' && !isNaN(value) && isFinite(value);
+		}
+
+		/**
+		 * @name toFixed
+		 * @param {number} value
+		 * @param {number} decimal
+		 * @return {number}
+		 * @since 2018-07-13
+		 */
+		function _toFixed(value, decimal) {
+			var result = NaN;
+			
+			//값이 정수일 때
+			if(_isInt(value)) {
+				result = value;
+				
+				//소수가 정수일 때
+				if(_isInt(decimal)) {
+					var splitValue = value.toString().split('.'),
+						splitValue1 = splitValue[1];
+					
+					//소수점이 있을 때
+					if(splitValue1) {
+						splitValue[1] = splitValue1.substring(0, decimal);
+						result = parseFloat(splitValue.join('.'), 10);
+					}
+				}
+			}
+
+			return result;
+		}
+
+		/**
 		 * @name calcScale
 		 * @since 2018-09-05
 		 * @param {number} from
@@ -15,28 +55,17 @@ try {
 		 */
 		window.calcScale = function(from, to) {
 			var result = 0;
-			
-			//숫자형으로 변환
-			from = parseFloat(from, 10);
-			to = parseFloat(to, 10);
 
-			//Infinity가 아니면서 0 이상일 때
-			if(isFinite(from) && from >= 0 && isFinite(to) && to >= 0) {
-				result /= from / to;
+			//정수이면서 0 이상일 때
+			if(_isInt(from) && from >= 0 && _isInt(to) && to >= 0) {
+				result = 1 / (from / to);
 
-				//NaN 또는 Infinity일 때
-				if(isNaN(result) || !isFinite(result)) {
+				//정수가 아닐 때
+				if(!_isInt(result)) {
 					result = to;
 				}
 
-				var splitResult = result.toString().split('.'),
-					splitResult1 = splitResult[1];
-				
-				//소수점이 있을 때
-				if(splitResult1) {
-					splitResult[1] = splitResult1.substring(0, 2);
-					result = parseFloat(splitResult.join('.'), 10);
-				}
+				result = _toFixed(result, 2);
 			}
 
 			return result;
