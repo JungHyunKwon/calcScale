f29900622d9d93a67fbcae89b0136b1a9da236c5
JungHyunKwon <kjh_3859@naver.com>
2019-01-24 09:30:46 +0900

    published

--- js/calcScale.js
+++ js/calcScale.js
@@ -26,7 +26,7 @@ try {
 		function _toFixed(value, decimal) {
 			var result = NaN;
 			
-			//값이 정수일 때
+			//값이 숫자일 때
 			if(_isNumber(value)) {
 				result = value;
 				
@@ -35,7 +35,7 @@ try {
 					var splitValue = value.toString().split('.'),
 						splitValue1 = splitValue[1];
 					
-					//소수점이 있을 때
+					//소숫점이 있을 때
 					if(splitValue1) {
 						splitValue[1] = splitValue1.substring(0, decimal);
 						result = parseFloat(splitValue.join('.'), 10);
@@ -56,11 +56,11 @@ try {
 		window.calcScale = function(from, to) {
 			var result = 0;
 
-			//정수이면서 0 이상일 때
+			//숫자이면서 0 이상일 때
 			if(_isNumber(from) && from >= 0 && _isNumber(to) && to >= 0) {
 				result = 1 / (from / to);
 
-				//정수가 아닐 때
+				//숫자가 아닐 때
 				if(!_isNumber(result)) {
 					result = to;
 				}
