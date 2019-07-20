9c3a75e4638c616bb6aca7e7d27481a54fce0a09
JungHyunKwon <kjh_3859@naver.com>
2019-07-21 02:08:04 +0900

    published

--- js/calcScale.js
+++ js/calcScale.js
@@ -2,75 +2,71 @@
  * @author JungHyunKwon
  * @version 1.0.0
  */
-try {
-	(function() {
-		'use strict';
+(function() {
+	'use strict';
 
-		/**
-		 * @name 숫자 확인
-		 * @since 2017-12-06
-		 * @param {*} value
-		 * @return {boolean}
-		 */
-		function _isNumeric(value) {
-			return typeof value === 'number' && !isNaN(value) && isFinite(value);
-		}
+	/**
+	 * @name 숫자 확인
+	 * @since 2017-12-06
+	 * @param {*} value
+	 * @return {boolean}
+	 */
+	function _isNumeric(value) {
+		return typeof value === 'number' && !isNaN(value) && isFinite(value);
+	}
 
-		/**
-		 * @name 소수점 절사
-		 * @param {number} value
-		 * @param {number} decimal
-		 * @return {number}
-		 * @since 2018-07-13
-		 */
-		function _toFixed(value, decimal) {
-			var result = NaN;
+	/**
+	 * @name 소수점 절사
+	 * @param {number} value
+	 * @param {number} decimal
+	 * @return {number}
+	 * @since 2018-07-13
+	 */
+	function _toFixed(value, decimal) {
+		var result = NaN;
 
-			//값이 숫자일 때
-			if(_isNumeric(value)) {
-				result = value;
+		//값이 숫자일 때
+		if(_isNumeric(value)) {
+			result = value;
+			
+			//소수가 정수일 때
+			if(_isNumeric(decimal)) {
+				var splitValue = value.toString().split('.'),
+					firstSplitValue = splitValue[1];
 				
-				//소수가 정수일 때
-				if(_isNumeric(decimal)) {
-					var splitValue = value.toString().split('.'),
-						firstOfSplitValue = splitValue[1];
-					
-					//소숫점이 있을 때
-					if(firstOfSplitValue) {
-						splitValue[1] = firstOfSplitValue.substring(0, decimal);
-						result = parseFloat(splitValue.join('.'), 10);
-					}
+				//소숫점이 있을 때
+				if(firstSplitValue) {
+					splitValue[1] = firstSplitValue.substring(0, decimal);
+					result = parseFloat(splitValue.join('.'), 10);
 				}
 			}
-
-			return result;
 		}
 
-		/**
-		 * @name calcScale
-		 * @since 2018-09-05
-		 * @param {number} from
-		 * @param {number} to
-		 * @return {number}
-		 */
-		window.calcScale = function(from, to) {
-			var result = 0;
+		return result;
+	}
 
-			//숫자이면서 0 이상일 때
-			if(_isNumeric(from) && from >= 0 && _isNumeric(to) && to >= 0) {
-				result = 1 / (from / to);
+	/**
+	 * @name calcScale
+	 * @since 2018-09-05
+	 * @param {number} from
+	 * @param {number} to
+	 * @return {number}
+	 */
+	window.calcScale = function(from, to) {
+		var result = 0;
 
-				//숫자가 아닐 때
-				if(!_isNumeric(result)) {
-					result = to;
-				}
+		//숫자이면서 0 이상일 때
+		if(_isNumeric(from) && from >= 0 && _isNumeric(to) && to >= 0) {
+			result = 1 / (from / to);
 
-				result = _toFixed(result, 2);
+			//숫자가 아닐 때
+			if(!_isNumeric(result)) {
+				result = to;
 			}
 
-			return result;
-		};
-	})();
-}catch(e) {
-	console.error(e);
-}
+			result = _toFixed(result, 2);
+		}
+
+		return result;
+	};
+})();
