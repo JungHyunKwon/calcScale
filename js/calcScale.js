1144d6c133d5bc470c73dddc50bdef3a6558edb8
JungHyunKwon <kjh_3859@naver.com>
2020-01-29 13:04:33 +0900

    published

--- js/calcScale.js
+++ js/calcScale.js
@@ -11,7 +11,7 @@
 	 * @param {*} value
 	 * @return {boolean}
 	 */
-	function _isNumeric(value) {
+	function isNumeric(value) {
 		return typeof value === 'number' && !isNaN(value) && isFinite(value);
 	}
 
@@ -22,21 +22,21 @@
 	 * @return {number}
 	 * @since 2018-07-13
 	 */
-	function _toFixed(value, decimal) {
+	function toFixed(value, decimal) {
 		var result = NaN;
 
-		//값이 숫자일 때
-		if(_isNumeric(value)) {
+		//숫자일 때
+		if(isNumeric(value)) {
 			result = value;
-			
-			//소수가 정수일 때
-			if(_isNumeric(decimal)) {
-				var split = value.toString().split('.'),
-					firstSplit = split[1];
+
+			//숫자일 때
+			if(isNumeric(decimal)) {
+				var split = result.toString().split('.'),
+					secondSplit = split[1];
 				
-				//소숫점이 있을 때
-				if(firstSplit) {
-					split[1] = firstSplit.substring(0, decimal);
+				//소수점이 있을 때
+				if(secondSplit) {
+					split[1] = secondSplit.substring(0, decimal);
 
 					result = parseFloat(split.join('.'));
 				}
@@ -56,16 +56,16 @@
 	window.calcScale = function(from, to) {
 		var result = 0;
 
-		//숫자이면서 0 이상일 때
-		if(_isNumeric(from) && from >= 0 && _isNumeric(to) && to >= 0) {
+		//숫자이면서 -1 보다 클 때
+		if(isNumeric(from) && from > -1 && isNumeric(to) && to > -1) {
 			result = 1 / (from / to);
 
 			//숫자가 아닐 때
-			if(!_isNumeric(result)) {
+			if(!isNumeric(result)) {
 				result = to;
 			}
 
-			result = _toFixed(result, 2);
+			result = toFixed(result, 2);
 		}
 
 		return result;
